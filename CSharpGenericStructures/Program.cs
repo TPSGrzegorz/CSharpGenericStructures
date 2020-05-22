d992b0f2099a6a188444ac996b8f73ffec0c9623
Grzegorz Bogusz <grzegorzb@outlook.com>
2020-05-22 10:33:09 +0200

    Refactoring

--- CSharpGenericStructures/Program.cs
+++ CSharpGenericStructures/Program.cs
@@ -9,19 +9,46 @@ namespace Railway
             var railway = new WheelRailway<double>(capacity: 3);
             var railway2 = new WheelRailway<int>(capacity: 4);
             var railway3 = new WheelRailway<string>(capacity: 1000);
-
             var railwayPerson = new WheelRailway<Person>();
-            railwayPerson.Write(new Person { Name = "Grzegorz", Surname = "Bogusz" });
-            railwayPerson.Write(new Person { Name = "Anna", Surname = "Bogusz" });
-            railwayPerson.Write(new Person { Name = "Tymoteusz", Surname = "Bogusz" });
-            railwayPerson.Write(new Person { Name = "Maryja", Surname = "Bogusz" });
 
+            CreatPersonRailway(railwayPerson);
+            PrintPersonRailway(railwayPerson);
+            ReadData(railway);
+            RefactoringData(railway);
+            Console.ReadLine();
+
+        }
+
+        private static void PrintPersonRailway(WheelRailway<Person> railwayPerson)
+        {
             while (!railwayPerson.IsEmpty)
             {
                 var person = railwayPerson.Read();
                 Console.WriteLine("\t\t" + person.Name + " " + person.Surname);
             }
+        }
 
+        private static void CreatPersonRailway(WheelRailway<Person> railwayPerson)
+        {
+            railwayPerson.Write(new Person { Name = "Grzegorz", Surname = "Bogusz" });
+            railwayPerson.Write(new Person { Name = "Anna", Surname = "Bogusz" });
+            railwayPerson.Write(new Person { Name = "Tymoteusz", Surname = "Bogusz" });
+            railwayPerson.Write(new Person { Name = "Maryja", Surname = "Bogusz" });
+        }
+
+        private static void RefactoringData(WheelRailway<double> railway)
+        {
+            var sum = 0.0;
+            Console.WriteLine("In our Railway:");
+            while (!railway.IsEmpty)
+            {
+                Console.WriteLine("\t\t" + railway.Read());
+            }
+            Console.WriteLine(sum);
+        }
+
+        private static void ReadData(WheelRailway<double> railway)
+        {
             while (true)
             {
                 var value = 0.0;
@@ -34,14 +61,6 @@ namespace Railway
                 }
                 break;
             }
-            var sum = 0.0;
-            Console.WriteLine("In our Railway:");
-            while (!railway.IsEmpty)
-            {
-                Console.WriteLine("\t\t" + railway.Read());
-            }
-            Console.WriteLine(sum);
-            Console.ReadLine();
         }
     }
 
