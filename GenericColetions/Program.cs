f08c56da9960432b7efd9de927d8fff423cb7018
Grzegorz Bogusz <grzegorzb@outlook.com>
2020-05-26 15:28:13 +0200

    HashSet

--- GenericColetions/Program.cs
+++ GenericColetions/Program.cs
@@ -11,29 +11,54 @@ namespace GenericColetions
     {
         static void Main(string[] args)
         {
+            //MyList();
 
-            //List<Worker> workers = new List<Worker>
-            //{
-            //    new Worker { Name = "Grzegorz", Sunrame = "Bogusz"},
-            //    new Worker { Name = "Jan", Sunrame = "Kowalski"},
-            //    new Worker { Name = "Jakub", Sunrame = "Nowak"},
-            //};
-            //workers[1].Name =  "Marek";
+            //MyQueue();
+            //MyStack();
 
-            ////Array.Resize(ref workers, 10);
+            MyHashSet();
+        }
 
-            ////workers[9] = new Worker { Name = "Anatol", Sunrame = "Koszyk" };
-            //workers.Add(new Worker { Name = "Anatol", Sunrame = "Koszyk" });
-            //foreach (var worker in workers)
-            //{
-            //    if (worker != null)
-            //        Console.WriteLine($"{worker.Name}, {worker.Sunrame}");
-            //}
+        private static void MyHashSet()
+        {
+            HashSet<Worker> set = new HashSet<Worker>();
+            var worker = new Worker { Name = "Adam", Sunrame = "Bogusz" };
+            set.Add(worker);
+            set.Add(worker);
+            set.Add(new Worker { Name = "Grzegorz", Sunrame = "Bogusz" });
+            set.Add(new Worker { Name = "Grzegorz", Sunrame = "Bogusz" }); ;
 
-            //var number = new List<int>(10);
-            //var capacity = -1;
 
-            //while (true)
+            foreach (var i in set)
+            {
+                Console.WriteLine(i.Name);
+            }
+        }
+
+        private static void MyList()
+        {
+            List<Worker> workers = new List<Worker>
+            {
+                new Worker { Name = "Grzegorz", Sunrame = "Bogusz"},
+                new Worker { Name = "Jan", Sunrame = "Kowalski"},
+                new Worker { Name = "Jakub", Sunrame = "Nowak"},
+            };
+            workers[1].Name = "Marek";
+
+            //Array.Resize(ref workers, 10);
+
+            //workers[9] = new Worker { Name = "Anatol", Sunrame = "Koszyk" };
+            workers.Add(new Worker { Name = "Anatol", Sunrame = "Koszyk" });
+            foreach (var worker in workers)
+            {
+                if (worker != null)
+                    Console.WriteLine($"{worker.Name}, {worker.Sunrame}");
+            }
+
+            var number = new List<int>(10);
+            var capacity = -1;
+
+            //while (true) // Test 
             //{
             //    if (number.Capacity != capacity)
             //    {
@@ -41,22 +66,11 @@ namespace GenericColetions
             //        Console.WriteLine(capacity);
             //    }
             //    number.Add(1);
-
-
             //}
-            //Console.ReadLine();
-            Console.WriteLine("!!!!!!!!!Queue!!!!!!!!!!!");
-            Queue<Worker> queue = new Queue<Worker>();
-            queue.Enqueue(new Worker { Name = "Grzegorz", Sunrame = "Bogusz" });
-            queue.Enqueue(new Worker { Name = "Anna", Sunrame = "Rurarz" });
-            queue.Enqueue(new Worker { Name = "Marek", Sunrame = "Cros" });
-            queue.Enqueue(new Worker { Name = "Ola", Sunrame = "War" });
+        }
 
-            while (queue.Count > 0)
-            {
-                var worker = queue.Dequeue();
-                Console.WriteLine(worker.Name + " " + worker.Sunrame);
-            }
+        private static void MyStack()
+        {
             Console.WriteLine("!!!!!!!!!!STOS!!!!!!!!!");
             Stack<Worker> stack = new Stack<Worker>();
             stack.Push(new Worker { Name = "Grzegorz", Sunrame = "Bogusz" });
@@ -70,7 +84,22 @@ namespace GenericColetions
                 var worker = stack.Pop();
                 Console.WriteLine(worker.Name + " " + worker.Sunrame);
             }
+        }
 
+        private static void MyQueue()
+        {
+            Console.WriteLine("!!!!!!!!!Queue!!!!!!!!!!!");
+            Queue<Worker> queue = new Queue<Worker>();
+            queue.Enqueue(new Worker { Name = "Grzegorz", Sunrame = "Bogusz" });
+            queue.Enqueue(new Worker { Name = "Anna", Sunrame = "Rurarz" });
+            queue.Enqueue(new Worker { Name = "Marek", Sunrame = "Cros" });
+            queue.Enqueue(new Worker { Name = "Ola", Sunrame = "War" });
+
+            while (queue.Count > 0)
+            {
+                var worker = queue.Dequeue();
+                Console.WriteLine(worker.Name + " " + worker.Sunrame);
+            }
         }
     }
 }
